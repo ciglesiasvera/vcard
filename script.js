b2045e7f4e961f2fa16b4692d524c7784a725073
ciglesiasvera <ciglesiasvera@gmail.com>
2024-03-09 11:56:44 -0300

    subida de archivos

---
+++ script.js
@@ -0,0 +1,4 @@
+function contactarPorWhatsapp() {
+    // Puedes redirigir al usuario a tu número de WhatsApp o realizar alguna acción específica aquí.
+    alert("¡Contacta conmigo por WhatsApp!");
+}
